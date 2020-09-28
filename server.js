1e243354ed9ec7f5217568cf5f71e474d039e210
Naemeh Adl <naemeh.adl@gmail.com>
2020-09-28 09:48:43 +0330

    ajab3

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ const db=knex({
     client: 'pg',
     connection: {
       connectionString : process.env.DATABASE_URL,
-     ssl: true
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 app.get('/',(req, res)=>{
