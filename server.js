4aa5cf95cd8a45816f25ede3c26de4025a16aaf0
Naemeh Adl <naemeh.adl@gmail.com>
2020-09-28 11:28:39 +0330

    api

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const saltRounds = 10;
 app.use(express.json());
 app.use(cors());
 const knex=require('knex');
-console.log('ttttttttttttttttttttt',process.env.DATABASE_URL);
 const db=knex({
     client: 'pg',
     connection: {
